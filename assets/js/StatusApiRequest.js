c86d2fae1672e67f2184d02267df0f86f3dfee24
Medieval Apple <evergaming2500@gmail.com>
2025-05-04 17:43:28 -0400

    Testing

--- assets/js/StatusApiRequest.js
+++ assets/js/StatusApiRequest.js
@@ -4,7 +4,7 @@ async function MakeStatusApiRequest(apiData)
 {
     const response = await fetch(host + apiData, {
         method: "GET", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
+        mode: "no-cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
         headers: {
             //"Content-Type": "application/json",
