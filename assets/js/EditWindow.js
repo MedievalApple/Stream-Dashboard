0adbec91b570a16cf41881b8b8c0cc5451a49b8c
Medieval Apple <114710036+MedievalApple@users.noreply.github.com>
2025-05-21 16:59:18 +0000

    Started work on Edit Screen

--- assets/js/EditWindow.js
+++ assets/js/EditWindow.js
@@ -1,13 +1,32 @@
 editMenu = document.getElementById("settings")
-triggers = [document.getElementById("trig0"),document.getElementById("trig1"),document.getElementById("trig2"),document.getElementById("trig3")]
+editOverlay = document.getElementById("edit-overlay");
+triggers = [document.getElementById("trig0"),document.getElementById("trig2"),document.getElementById("trig1"),document.getElementById("trig3")]
 windows = [document.getElementById("settings"),document.getElementById("settings"),document.getElementById("settings"),document.getElementById("settings")]
 
-triggers.forEach(trigger => {
+addEventListener("keypress", (key) => { 
+    console.log(key)
+    if (key.key == "e"){
+        if(editOverlay.style.display == "none"){
+            editOverlay.style.display = "grid"
+        } else {
+            editOverlay.style.display = "none"
+            hideEditWindow()
+        }
+    }
+})
+
+triggers.forEach((trigger, i) => {
     trigger.addEventListener("contextmenu", (e) => {
         e.preventDefault();
+        showEditWindow(i)
     });
 });
 
-function editWindow(num) {
+function showEditWindow(num) {
+    console.log("Window: " + num)
     editMenu.hidden = false
+}
+
+function hideEditWindow() {
+    editMenu.hidden = true;
 }
